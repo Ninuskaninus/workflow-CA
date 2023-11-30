5efa3417db22af921eca11b440eadc180795e560
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2023-11-30 12:21:02 +0100

    more edits in unit testing

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -2,7 +2,11 @@
 import 'jest-localstorage-mock';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
+import fetch from 'node-fetch'; // Import node-fetch
 const { login } = require('./login.js');
+
+// Mock fetch
+jest.mock('node-fetch', () => require('jest-fetch-mock'));
 
 describe('login function', () => {
   const mock = new MockAdapter(axios, { delayResponse: 1000 });
@@ -15,14 +19,18 @@ describe('login function', () => {
   it('should store a valid token in localStorage after login', async () => {
     const email = 'ninamd@stud.noroff.no';
     const password = 'ninaamdal12345';
+
+    // Mock the fetch response
+    fetch.mockResponseOnce(JSON.stringify({ authToken: 'testAuthToken' }));
+
     await login(email, password);
 
-    // Expect that localStorage.getItem("token") returns a non-empty string
     const storedToken = localStorage.getItem('token');
     expect(storedToken).toBeTruthy();
   });
 
   afterAll(() => {
     mock.restore();
+    jest.unmock('node-fetch'); // Unmock fetch after all tests
   });
 });
