01a3903b690aef631b5d437c64e20c014949a51b
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2023-11-30 10:34:59 +0100

    Gone over feedback and made changes

--- cypress/e2e/cannotSubmit.cy.js
+++ cypress/e2e/cannotSubmit.cy.js
@@ -1,12 +1,23 @@
-describe("User Login and Profile Access", () => {
-  it("should log in and access the user profile", () => {
-    cy.visit("index.html");
+describe('User Login and Profile Access', () => {
+  it('should log in and access the user profile', () => {
+    cy.visit('index.html');
     cy.wait(2000);
     cy.get("button[data-bs-target='#loginModal']:visible").click();
 
     cy.wait(2000);
-    cy.get("input[type='email']:visible").type("wrong@stud.noroff.no");
-    cy.get("input[type='password']:visible").type("wrongpassword");
+    cy.get("input[type='email']:visible").type('wrong@stud.noroff.no');
+    cy.get("input[type='password']:visible").type('wrongpassword');
     cy.get('button[type="submit"]:visible').click();
+
+    cy.on('window:alert', (text) => {
+      expect(text).to.include(
+        'Either your username was not found or your password is incorrect',
+      );
+    });
+
+    cy.window()
+      .its('localStorage')
+      .invoke('getItem', 'token')
+      .should('not.exist');
   });
 });
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,15 +1,14 @@
-describe("User Login and Profile Access", () => {
-  it("should log in and access the user profile", () => {
-    // Visit the login page (adjust the URL as needed).
-    cy.visit("index.html");
+describe('User Login and Profile Access', () => {
+  it('should log in and access the user profile', () => {
+    cy.visit('index.html');
     cy.wait(2000);
     cy.get("button[data-bs-target='#loginModal']:visible").click();
 
-    // Fill in the login form with valid credentials.
-
     cy.wait(2000);
-    cy.get("input[type='email']:visible").type("ninamd@stud.noroff.no");
-    cy.get("input[type='password']:visible").type("ninaamdal12345");
+    cy.get("input[type='email']:visible").type('ninamd@stud.noroff.no');
+    cy.get("input[type='password']:visible").type('ninaamdal12345');
     cy.get('button[type="submit"]:visible').click();
+
+    cy.window().its('localStorage').invoke('getItem', 'token').should('exist');
   });
 });
