c69ce42dfc9b09192f388d432421f00e10b4383a
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2023-11-30 12:13:49 +0100

    edited the unit testing

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,9 +1,17 @@
-const { logout } = import("./logout.js");
-describe("logout function", () => {
-  it("should clear the token from localStorage", () => {
-    localStorage.setItem("authToken", "test-token");
+// logout.test.js
+import 'jest-localstorage-mock';
 
+const { logout } = require('./logout.js');
+
+describe('logout function', () => {
+  it('should clear the token from localStorage', () => {
+    // Set a test token in localStorage
+    localStorage.setItem('token', 'test-token');
+
+    // Call the logout function
     logout();
-    expect(localStorage.getItem("authToken")).toBeNull();
+
+    // Expect that localStorage.getItem("token") returns null after logout
+    expect(localStorage.getItem('token')).toBeNull();
   });
 });
